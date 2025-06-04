7fa30dc12398c98dbd544c0317282a0ef6c9dc5b
[dyad] <greenzonegames@outlook.com>
2025-06-04 01:35:09 -0500

    [dyad] wrote 1 file(s)

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,16 +1,12 @@
+"use client";
 
-import React from "react"
-import { Toaster as Sonner, toast } from "sonner"
-import { useTheme } from "@/components/theme-provider"
+import { Toaster as Sonner, toast } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
       className="toaster group"
       toastOptions={{
         classNames: {
@@ -25,7 +21,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster, toast }
+export { Toaster, toast };
